d6237cdefe543ff0e6ba9a4bd0e73db52cf40520
CharretQuentin <quentin.charret@etu.univ-lyon1.fr>
2018-11-22 16:04:25 +0100

    implémentation de la 1 ère version de la fonction qui calcul la variance

--- mean.py
+++ mean.py
@@ -2,9 +2,13 @@
 # passée en paramètre
 def Mean(list_float):
 	
+# Fonction qui calcul la variance d'une liste de float
+# passée en paramètre	
+def Var(list_float):
 	
 # main
 def main():
 	x = [1,2,3]
 	Mean(list_float)
+	Var(list_float)
 main()
